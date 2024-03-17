b76e155616816f84a4a2c8af93dd6683261dd306
Atanas Vasilev <my.atanas.vasilev@gmail.com>
2024-03-17 10:39:14 +0200

    Initial setup incl NextUI + Tailwind

---
+++ src/app/about/page.tsx
@@ -0,0 +1,5 @@
+import { Button } from "@nextui-org/react";
+
+export default function AboutPage() {
+  return <Button color="primary">Button</Button>;
+}
---
+++ src/app/newTest/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@nextui-org/react";
+
+const NewTest = () => {
+  return <Button color="danger">Danger</Button>;
+};
+
+export default NewTest;
